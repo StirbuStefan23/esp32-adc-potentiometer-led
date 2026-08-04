286534000976a1c98921a93502a71146b6d3e040
Stirbu Stefan <stefan.stirbu@student.tuiasi.ro>
2026-08-04 19:12:48 +0300

    Get raw input from potentiometer

--- POT_ADC_LED.ino
+++ POT_ADC_LED.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#define POT_PIN 34
 
+int potValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(POT_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Citim valoarea (0 - 4095)
+  potValue = analogRead(POT_PIN);
+  //Calculam tensiunea (0 - 3.3V)
+  float voltage = (potValue * 3.3) / 4095.0; 
 
+  Serial.print("Raw value ADC: ");
+  Serial.println(potValue);
+
+  Serial.print("Voltage: ");
+  Serial.print(voltage);
+  Serial.println(" V");
+
+  delay(200);
 }
